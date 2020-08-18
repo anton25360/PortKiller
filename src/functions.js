f17471e2e29d6ad1068e173cdb8d4f1f0f8cb383
Anton Webb <anton25360@gmail.com>
2020-08-18 17:13:33 +0100

    result message now changes programatically

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,15 @@
 function logPortValue() {
     let port = document.getElementById('portName').value
     console.log(port);
+    setResult(false)
     event.preventDefault()
   }
+
+function setResult(input) {
+
+    if (input) { //istrue
+        document.getElementById('result').innerHTML = '<p class="green">yes</p>'
+    } else {
+        document.getElementById('result').innerHTML = '<p class="red">no</p>'
+    }
+}

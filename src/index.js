7a6b7dd96e5089cf62f132e8acab73162857be3d
Anton Webb <anton25360@gmail.com>
2020-08-18 16:14:17 +0100

    Added seperate functions.js file

--- src/index.js
+++ src/index.js
@@ -44,3 +44,12 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+// document.getElementById('portBtn').onsubmit() {
+//   console.log('helo');
+// }
+
+// function alertMe() {
+//   alert('throne')
+//   console.log('hello');
+// }
